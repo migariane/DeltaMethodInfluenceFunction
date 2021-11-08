398f1a214e2234a17876d03d6278436752ca1a5b
migariane <watzilei@hotmail.com>
2021-11-08 12:07:52 +0100

    update

---
+++ CodeBoxes.R
@@ -0,0 +1,159 @@
+
+## BOX ONE
+# Data generation
+set.seed(7777)
+n <- 1000
+y <- runif(n, 0, 1)
+theoretical_mu <- 0.5 
+empirical_mu <- mean(y)
+# Functional delta-method: influence curve for the sample mean (first derivative=1(constant))
+IF <- 1 * (y - empirical_mu)
+mean(IF) #zero by definition
+# Plug-in estimation of the sample mean
+Yhat <- y + IF # Plug-in estimator
+mean(Yhat)
+# Geometry of the IF
+plot(y, IF)
+# Standard Error: Influence Fuction
+varYhat.IF <- var(IF) / n # hatvar(hatIF) =1/n sum(yi-bary)^2
+seIF <- sqrt(varYhat.IF);seIF
+# Asymptotic linear inference 95% Confidence Intervals
+Yhat_95CI <- c(mean(Yhat) - qnorm(0.975) * sqrt(varYhat.IF), mean(Yhat) + qnorm(0.975) * sqrt(varYhat.IF)); 
+mean(Yhat)
+## [1] 0.508518
+Yhat_95CI
+## [1] 0.490561 0.526475
+
+# Check with implemented delta-method in library msm 
+library(msm)
+se <- deltamethod(g = ~ x1, mean = empirical_mu, cov = varYhat.IF)
+se
+## [1] 0.009161893
+
+# Check 95%CI delta-method computed by hand with delta-method implemented in RcmdrMisc library
+library(RcmdrMisc)
+DeltaMethod(lm(y ~ 1), "b0")
+#       Estimate SE           2.5%   97.5%
+#    b0 0.508518 0.009161893 0.490561 0.526475
+
+## BOX TWO
+# Delta-method for the SE of the correlation between two vectors U and V based on the IF.
+#install.packages("psychometric")
+#install.packages("MASS")
+library(psychometric)
+library('MASS')
+
+# Generate the data
+samples = 1000
+R = 0.83
+library('MASS')
+data = mvrnorm(n=samples, mu=c(0, 0), Sigma=matrix(c(1, R, R, 1), nrow=2), empirical=TRUE)
+U = data[, 1]  # standard normal (mu=0, sd=1)
+V = data[, 2]  # standard normal (mu=0, sd=1)
+# Assess that it works
+cor(U, V)  # r = 0.83
+
+mu1 = mean(U*V)
+mu2 = mean(U)
+mu3 = mean(V)
+mu4 = mean(U^2)
+mu5 = mean(V^2) 
+
+IC1 = U*V-mu1 
+IC2 = U-mu2
+IC3 = V-mu3 
+IC4 = U^2-mu4 
+IC5 = V^2-mu5
+
+IC = 
+    (sqrt(mu4-mu2^2)*sqrt(mu5-mu3^2))^(-1)*IC1+ 
+    (-mu3*sqrt(mu4-mu2^2)*sqrt(mu5-mu3^2)+(mu1-mu2*mu3)*mu2*sqrt(mu5-mu3^2)/sqrt(mu4-mu2^2))/((mu4-mu2^2)*(mu5-mu3^2))*IC2+ 
+    (-mu2*sqrt(mu4-mu2^2)*sqrt(mu5-mu3^2)+(mu1-mu2*mu3)* mu3*sqrt(mu4-mu2^2)/sqrt(mu5-mu3^2))/((mu4-mu2^2)*(mu5-mu3^2))*IC3+ 
+    (-mu1+mu2*mu3)/(2*(mu4-mu2^2)^1.5*(mu5-mu3^2)^.5)*IC4+ 
+    (-mu1+mu2*mu3)/(2*(mu4-mu2^2)^.5*(mu5-mu3^2)^1.5)*IC5
+
+SE = sd(IC)/sqrt(1000); SE
+
+rho_hat = (mu1-mu2*mu3)/(sqrt(mu4-mu2^2)*sqrt(mu5-mu3^2)); rho_hat
+CI = c(rho_hat-1.96*SE,rho_hat+1.96*SE); CI
+## CI [1] 0.8106 0.8493
+
+## Using the function CIr from the psychometric package in R: 
+CIr(rho_hat,1000,.95)
+## [1] 0.8096 0.8483
+
+## BOX THREE: Data generation (simulated example) to apply the Delta-method in a multiple regression 
+# Data generation
+set.seed(05061972)
+N <- 1000
+# Age (1: > 65; 0: <= 65)
+age <- rbinom(N,1,0.6)                                    
+# SES (1: middle class and higher)
+SES <- rbinom(N,1,plogis(0.97 + 0.03*age))  
+# comorbidities (1: any, 0: none)
+morbidity <- rbinom(N,1,plogis(0.7 + 0.010*age - 0.15*SES))     
+# stage (1: advanced, TNS=3 or 4; 0: TNS= 1 or 2)
+stage <- rbinom(N,1,plogis(-1.0 + 0.1*age - 0.05*SES + 0.2*morbidity)) 
+# treatment (1: dual; 0=mono)
+treat <- rbinom(N,1,plogis(0.35 + 0.015*stage - 0.15*age + 0.015*SES - 0.3*morbidity)) 
+# Counterfactual outcome under A=1 and A=0 respectively
+death.1 <-  rbinom(N,1,plogis(-2 - 1*1 + 0.65*age - 0.025*SES + 0.25*morbidity + 0.75*stage + 0.025*1*SES - 0.35*1*morbidity))
+death.0 <-  rbinom(N,1,plogis(-2 - 1*0 + 0.65*age - 0.025*SES + 0.25*morbidity + 0.75*stage + 0.025*0*SES - 0.35*0*morbidity))
+# Observed outcome: mortality at 1 year after treatment initiation (1: death)
+death <- death.1*treat + death.0*(1 - treat)
+# OR -> if treated, lower prob of death
+exp(coefficients(glm(death~treat,family=binomial)))
+# RR
+exp(coefficients(glm(death ~ treat + age + SES + morbidity + stage,family=poisson)))
+# risk differences
+mean(death.1-death.0)
+## -0.124
+
+## BOX FOUR
+# Delta method to derive the SE for the conditional RR
+data  <- as.data.frame(cbind(death , treat , age , SES , morbidity , stage))
+m1 <- glm(death ~ age + treat, family = binomial, data = data)
+pMono <- predict(m1, newdata = data.frame(age = 1, treat = 0), type = "response")
+pDual <- predict(m1, newdata = data.frame(age = 0, treat = 1), type = "response")
+rr <- pMono / pDual
+cat("Conditional risk ratio: ", rr)
+# Conditional Risk Ratio:  4.406118
+
+# The partial derivative are computed in R as follows:
+x1 <- 1
+x2 <- 0
+x3 <- 1
+x4 <- 0
+b0 <- coef(m1)[1]
+b1 <- coef(m1)[2]
+b2 <- coef(m1)[3]
+e1 <- exp(- b0 - 1 * b1 - 0 * b2)
+e2 <- exp(- b0 - 0 * b1 - 1 * b2)
+p1 <- 1 / (1 + e1)
+p2 <- 1 / (1 + e2)
+dfdb0 <- - e2 * p1 + (1 + e2) * p1 * (1 - p1)
+dfdb1 <- - x2 * e2 * p1 + (1 + e2) * x1 * p1 * (1 - p1)
+dfdb2 <- - x4 * e2 * p1 + (1 + e2) * x3 * p1 * (1 - p1)
+grad <- c(dfdb0, dfdb1, dfdb2)
+vG <- t(grad) %*% vcov(m1) %*% (grad)
+se_rr <- c(sqrt(vG))
+se_rr
+## [1] 1.038303
+
+# Check with implemented delta-method in library msm 
+library(msm)
+se_rr_delta <- deltamethod( ~ (1 + exp(- x1 - 0 * x2 - 1 * x3)) /
+                                (1 + exp(- x1 - 1 * x2 - 0 * x3)), 
+                            c(b0, b1, b2), 
+                            vcov(m1)
+)
+se_rr_delta
+## [1] 1.038303
+# We obtain the same results for the SE of the RR computed before
+
+# Finally, we compute the type Wald 95% CI
+lb <- rr - 1.96 * sqrt(vG)
+ub <- rr + 1.96 * sqrt(vG)
+# Conditional Risk Ratio (95%CI)
+c(lb, ub)
+## [1] 2.386165 6.426072
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# DeltaMethodInfluenceFunction
-Statistical Inference 
+## The Delta-method and influence function in epidemiology: areproducible tutorial
+
+### Authors
+Rodrigo Zepeda-Tello 1,2| Michael Schomaker 3,4| Aurelien Belot 5| Bernard Rachet 5| Mireille E.Schnitzer 6,7| Miguel Angel Luque Fernandez*5,8,91
+
+### Affiliations 
+1. Instituto Tecnologico Autonomo de Mexico, Mexico.
+2. Instituto Mexicano del Seguro Social, Mexico.
+3. Institute of Public Health, Medical Decision Making and Health Technology Assessment Department of Public Health, Health Services Research and Health Technology Assessment UMIT - University for Health Sciences, Medical Informatics and Technology, Austria.
+4. Centre for Infectious Disease Epidemiology and Research, University of Cape Town,South Africa.
+5. ICON-group. Non-communicable Disease Epidemiology. London School of Hygiene and Tropical Medicine. London, U.K.
+6. Faculty of Pharmacy and Department of Social and Preventive Medicine, University of Montreal, Montreal, Canada.
+7. Department of Epidemiology, Biostatistics and Occupational Health, McGill University, Montreal, Canada.
+8. Non-communicable Disease and Cancer Epidemiology Group, Instuto Biosanitario de Granada (ibs.GRANADA), Andalusian School of Public Health, University o fGranada, Granada, Spain.
+9. Biomedical Network Research Centers of Epidemiology and Public Health(CIBERESP), Madrid, Spain.
+
+Correspondence* Miguel Angel Luque-Fernandez, Email: miguel-angel.luque@lshtm.ac.uk  
+
+This repository makes available to the scientific community the data and code used in the manuscript available at  
+
+[Link to the published article]()
+
+### ABSTRACT
+Approximate statistical inference via determination of the asymptotic distribution of statistics is routinely used for inference in applied medical statistics(e.g., risk ratio standard error). One method for variance estimation is the classical Delta method. But there is a gap as it is not routinely included in training for applied medical statistics and many researchers do not understand it or its uses. Given that a smooth function of an asymptotically normal estimator is also asymptotically normally distributed, the Delta method offers a formula to obtain the large-sample variance of a function of an estimator with known large-sample properties. In a more general setting, it is a technique for approximating the variance of a functional i.e., an estimand that takes a function as an input and applies another function to it (e.g.the expectation function). Therefore, we may approximate the distribution using the functional Delta method based on the influence function. The ongoing development of new methods and techniques may pose a challenge for applied statisticians who are interested in mastering the application of these methods. In this tutorial we introduce the use of the classical and functional Delta method and their links to the influence curve from a practical perspective. The code can be accessed at 
+
+[https://github.com/migariane/DeltaMethodInfluenceFunction](https://github.com/migariane/DeltaMethodInfluenceFunction)  
+
+KEYWORDS:Statistical inference; Delta method; Epidemiology; Tutorial
